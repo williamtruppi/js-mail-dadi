03c0bc70030ae90d0d0435f32c0d1997b5c7f8dd
William Truppi <williamt@hotmail.it>
2020-11-17 17:54:56 +0100

    Aggiunti i commenti al JS dell'esercizio sui dadi

--- Dadi/assets/js/main.js
+++ Dadi/assets/js/main.js
@@ -1,27 +1,21 @@
+//creo le due variabili che conterranno i numeri casuali da 1 a 6
 var userScore = Math.floor((Math.random()* 6 ) + 1);
 var cpuScore = Math.floor((Math.random()* 6 ) + 1);
 
+//creo una variabile che conterrà il risultato del gioco
 var message;
-var victory, draw, defeat = 0;
 
+//controllo delle due variabili generate casualmente
 if (userScore > cpuScore){
   message = "Hai vinto " + userScore + " a " + cpuScore;
-  victory++;
 } else if (userScore == cpuScore) {
   message = "Hai pareggiato " + userScore + " a " + cpuScore;
-  draw++;
 } else {
   message = "Hai perso " + userScore + " a " + cpuScore;
-  defeat++;
 }
 
+//invio del risultato al codice html
 var messageOutput = document.getElementById("message");
 messageOutput.innerHTML = message;
 
-/* var victoryOutput = document.getElementById("victory");
-var drawOutput = document.getElementById("draw");
-var defeatOutput = document.getElementById("defeat");
-victoryOutput.innerHTML = victory;
-drawOutput.innerHTML = draw;
-defeatOutput.innerHTML = defeat;
- */
+
