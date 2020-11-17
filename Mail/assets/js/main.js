19cd76e413d8ba5bbf9c8ae0e1d776ae7766c6f1
William Truppi <williamt@hotmail.it>
2020-11-17 16:25:20 +0100

    esercizio completo con interazione con gli input

--- Mail/assets/js/main.js
+++ Mail/assets/js/main.js
@@ -2,21 +2,40 @@
 var mailList = ["batman@gmail.com", "superman@hotmail.it", "iamironman@libero.it"];
 console.log(mailList);
 
-//acquisizione email da confrontare
-var emailInput = document.getElementById("user_email");
-console.log(emailInput);
+//creazione variabile per il btn 
+var btnInvio = document.getElementById("btn_invio");
 
+// dichiarazione di una variabile messaggio da mostrare a schermo
+var message;
+var emailFound;
 
+btnInvio.addEventListener("click", function (){
 
-// ciclo for per effettuare il controllo della lista
-for (var i = 0; i <= 2; i++) {
-  
-  if (emailInput == mailList[i]){
-    console.log("La tua email è presente nella lista");
-    break;
-  } else {
-    console.log("la tua email NON è presente nella lista");
+  //acquisizione email da confrontare
+  var emailInput = document.getElementById("user_email").value;
+  console.log(emailInput);
+
+  // ciclo for per effettuare il controllo della lista
+  for (var i = 0; i <= mailList.length; i++) {
+    
+    if (emailInput == mailList[i]){
+      emailFound = true;
+      break;
+    } else {
+      emailFound = false
+    }
   }
-}
+
+
+  if (emailFound == true) {
+    message = "L'email per il ripristino della password è stata inviata";
+  } else {
+    message = "email non trovata";
+  }
+
+  var messageOutput = document.getElementById("message");
+  messageOutput.innerHTML = message;
+
+});
 
 console.log("il ciclo è terminato");
